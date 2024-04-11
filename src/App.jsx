db43145e8a256ae5e57bd7a680ebb7f03433ed49
sagorchowdhury <rajchowdhury820@gmail.com>
2024-04-11 20:54:03 +0400

    setupProjects

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl p-4'>hello world</h1>
+      <h6 className='text-3xl p-4'>hello world</h6>
+    </>
+  )
+}
+
+export default App
